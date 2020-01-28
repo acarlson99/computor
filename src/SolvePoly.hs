32e648d81baed2c65f0902fb71cd371329c28e01
Alexander Carlson <bombblob@gmail.com>
2020-01-27 18:50:32 -0800

    Added strToTerm, splitOn, made solvePoly return tuple of (lhs,rhs)

--- src/SolvePoly.hs
+++ src/SolvePoly.hs
@@ -1,6 +1,8 @@
 module SolvePoly
     ( solvePoly
     , splitExpr
+    , rgxFilter
+    , splitOn
     ) where
 
 import Data.List
@@ -24,17 +26,34 @@ data PolyAnswer = PolyAnswer String Float Float deriving (Show)
 maybeToEither _ (Just a) = Right a
 maybeToEither est Nothing = Left est
 
--- getAllTextMatches  $ "1*X^3 + 2*X^7" =~ "[[:digit:]]+\\*X\\^[[:digit:]]+" :: [String]
+stripWhitespace :: String -> String
+stripWhitespace = foldl (++) "" . words
+
+-- "1*X^3 + 2*X^7" =~ "[[:digit:]]+\\*X\\^[[:digit:]]+" :: [[String]]
+
+-- ~ rgxFilter :: String -> [[String]]
+-- ~ rgxFilter xs = filter (\a -> a /= ["","","","",""]) $ xs =~ "(([[:digit:]]+)\\*)?(X\\^([[:digit:]])+)?"
+rgxFilter :: String -> [String]
+rgxFilter xs = getAllTextMatches $ stripWhitespace xs =~ "[+\\-]? ?[[:digit:]]+(\\.[[:digit:]]+)?\\*X\\^-?[[:digit:]]+"
 
 -- Split expression "1 * X^3 + 2 * X^2 = X^1.5"
 --             into (["1 * X^3", "2 * X^2"], ["X^1.5"])
-splitExpr :: String -> Either String ([String], [String])
+splitExpr :: String -> Either String (String, String)
 splitExpr expr = do
     let exprs = words expr
     idx <- maybeToEither "Unable to find '=' sign" $ elemIndex "=" exprs
-    return (take idx exprs, drop (idx + 1) exprs)
+    return (unwords $ take idx exprs, unwords $ drop (idx + 1) exprs)
+
+splitOn :: Char -> String -> (String, String)
+splitOn _ [] = ("","")
+splitOn c (x:xs)
+    | c == x = ("",xs)
+    | otherwise = (x:lhs,rhs)
+                  where (lhs,rhs) = splitOn c xs
 
 -- solvePoly :: String -> Either String PolyAnswer
 solvePoly expr = do
-    (lhs, rhs) <- splitExpr expr
-    return (lhs, rhs)
+    -- ~ (lhs, rhs) <- splitExpr expr
+    let (lhs, rhs) = splitOn '=' expr
+        f = map strToTerm . rgxFilter
+    return (f lhs, f rhs)
--- src/Term.hs
+++ src/Term.hs
@@ -2,7 +2,10 @@ module Term
     ( Term (..)
     , addTerm
     , subTerm
+    , strToTerm
     ) where
+
+import Text.Regex.Posix
 
 data Term = Term { coef :: Float, exp :: Int }
 
@@ -18,3 +21,8 @@ subTerm :: Term -> Term -> Maybe Term
 subTerm (Term c1 f1) (Term c2 f2)
     | f1 == f2  = Just $ Term (c1 - c2) f1
     | otherwise = Nothing
+
+-- strToTerm "1*X^3" :: Term
+strToTerm :: String -> Term
+strToTerm xs = Term (read c) (read f)
+          where [[_,c,_,f]] = xs =~ "(-?[[:digit:]]+(\\.?[[:digit:]]+)?)\\*X\\^(-?[[:digit:]]+)" :: [[String]]
