5914472b701fad18c1dc78bbbf272c8b63804e56
Alexander Carlson <bombblob@gmail.com>
2020-02-03 12:46:10 -0800

    Added matrix, test, made Complex a functor

--- src/Types.hs
+++ src/Types.hs
@@ -2,15 +2,21 @@ module Types
     ( Complex (..)
     ) where
 
+import Data.Matrix
+
 newtype Complex a = Complex (a,a) deriving (Eq)
 
 instance (Floating a) => Num (Complex a) where
     Complex (a,b) + Complex (c,d) = Complex (a+c, b+d)
     Complex (a,b) - Complex (c,d) = Complex (a-c, b-d)
     Complex (a,b) * Complex (c,d) = Complex (a*c - b*d, a*d + b*c)
+
     abs (Complex (a,b)) = Complex (sqrt ((a^2) + (b^2)), 0)
     signum (Complex (a,b)) = Complex (signum a, signum b)
     fromInteger i = Complex (fromInteger i, 0)
+
+instance Functor Complex where
+    fmap f (Complex (a,b)) = Complex(f a, f b)
 
 instance (Show a, Num a, Ord a) => Show (Complex a) where
     show (Complex (a,b))
