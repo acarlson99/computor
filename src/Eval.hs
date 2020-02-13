159a4f8b0a6e5dec22775da4694358401e7488a8
Alexander Carlson <bombblob@gmail.com>
2020-02-12 17:31:58 -0800

    Defining evalExpr

--- src/Eval.hs
+++ src/Eval.hs
@@ -17,25 +17,27 @@ data CalcState = CalcState { getFuncs :: M.Map String (Ident, [Ident], Expr)
 emptyState :: CalcState
 emptyState = CalcState M.empty M.empty
 
--- ~ evalPrim :: Num a => Primitive -> CalcState -> Either String a
--- ~ evalPrim (Number n)             st = Right n
--- ~ evalPrim (Float f)              st = Right f
--- ~ evalPrim (Identifier (Ident s)) st = do
-    -- ~ exp <- maybeToEither ("Unable to find identifier: " ++ s) $ M.lookup s $ getIdent st
-    -- ~ evalExpr exp
-evalPrim (Identifier (Ident s)) st = do
-    exp <- maybeToEither ("Unable to find identifier: " ++ s) $ M.lookup s $ getIdent st
-    evalExpr exp
--- ~ evalPrim (Complex c)            st = Right c
--- ~ evalPrim (Complex (T.Complex (r,c)))            st = return $ T.Complex (r,c)
-
 -- ~ evalExpr :: (Show a, Num a) => Expr -> Either String a
-evalExpr (Primitive' n) = return n
-evalExpr exp = Left $ "unimplemented: " ++ show exp
+evalExpr st (Primitive' n) = return $ Primitive' n
+evalExpr st (Identifier (Ident idnt)) = maybeToEither ("variable undefined: " ++ idnt) $ M.lookup idnt $ getIdent st
+evalExpr st (Array xs) = Array <$> traverse (evalExpr st) xs
+evalExpr st (Matrix xs) = Matrix <$> mapM (evalExpr st) xs
+-- NOTE: cannot test currently
+evalExpr st (Funcall (Fcall (Ident idnt,xs))) = do
+    (id,xs,body) <- maybeToEither ("function undefined: " ++ idnt) $ M.lookup idnt $ getFuncs st
+    -- TODO: assign variables in scope
+    evalExpr st body
+evalExpr st (Operation (op,lhs,rhs)) = do
+    lhs' <- evalExpr st lhs
+    rhs' <- evalExpr st rhs
+    -- TODO: properly assign variables
+    Left $ show op ++ " " ++ show lhs' ++ " " ++ show rhs'
+-- ~ evalExpr n _ = return n
+-- ~ evalExpr st exp = Left $ "unimplemented: " ++ show exp
 
 eval :: [(ParseTree,String)] -> CalcState -> (CalcState, IO ())
 eval []               st = (st, return ())
 eval [(expr,x:xs)]    st = (st, putStrLn $ "ERROR: unrecognized tokens: '" ++ (x:xs) ++ "' in expression '" ++ show expr ++ "'")
 eval (x:y:ys)         st = (st, putStrLn $ "WAIT WTF THIS SHOULD NOT HAPPEN" ++ show (x:y:ys))
-eval [(Expr' expr,_)] st = (st, print $ evalExpr expr)
+eval [(Expr' expr,_)] st = (st, print $ evalExpr st expr)
 eval expr             st = (st, print expr)
