c63324a815776905f759af5c3fe5d2e33f99206e
Alexander Carlson <bombblob@gmail.com>
2020-02-13 16:21:41 -0800

    Updated evalExpr to evaluate from parse tree to base type, updated TODO.yaml

--- src/Eval.hs
+++ src/Eval.hs
@@ -10,6 +10,15 @@ import qualified Types as T
 
 import Parse.Types
 
+-- ~ import Eval.Types
+
+data BaseType = Int Int
+              | Flt Float
+              | Cpx (T.Complex Float)
+              | Arr [BaseType]
+              | Mtx [BaseType]
+              deriving (Show,Eq)
+
 data CalcState = CalcState { getFuncs :: M.Map String ([Ident], Expr)
                            , getVars :: M.Map String Expr }
 
@@ -23,12 +32,23 @@ instance Show CalcState where
 emptyState :: CalcState
 emptyState = CalcState M.empty M.empty
 
--- ~ evalExpr :: (Show a, Num a) => Expr -> Either String a
-evalExpr st (Primitive' n) = return $ Primitive' n
-evalExpr st (Identifier (Ident idnt)) = maybeToEither ("variable undefined: " ++ idnt) $ M.lookup idnt $ getVars st
-evalExpr st (Array xs) = Array <$> traverse (evalExpr st) xs
-evalExpr st (Matrix xs) = Matrix <$> mapM (evalExpr st) xs
--- NOTE: cannot test currently
+
+
+applyOp Add (Int lhs) (Int rhs) = return $ Int $ lhs + rhs
+applyOp op lhs rhs = Left $ "Unimplemented instruction: " ++ show op ++ " " ++ show lhs ++ " " ++ show rhs
+
+evalExpr :: CalcState -> Expr -> Either String BaseType
+evalExpr _ (Primitive' n) = case n of
+                            (Number n) ->  return $ Int n
+                            (Float n)  ->  return $ Flt n
+                            (Complex n) -> return $ Cpx n
+evalExpr st (Identifier (Ident idnt)) = do
+    v <- maybeToEither ("variable undefined: " ++ idnt) $ M.lookup idnt $ getVars st
+    evalExpr st v
+
+evalExpr st (Array xs) = Arr <$> traverse (evalExpr st) xs
+evalExpr st (Matrix xs) = Mtx <$> mapM (evalExpr st) xs
+
 evalExpr st (Funcall (Fcall (Ident idnt,xs))) = do
     (args,body) <- maybeToEither ("function undefined: " ++ idnt) $ M.lookup idnt $ getFuncs st
     -- TODO: assign variables in scope
@@ -36,19 +56,22 @@ evalExpr st (Funcall (Fcall (Ident idnt,xs))) = do
 evalExpr st (Operation (op,lhs,rhs)) = do
     lhs' <- evalExpr st lhs
     rhs' <- evalExpr st rhs
-    -- TODO: properly assign variables
-    Left $ show op ++ " " ++ show lhs' ++ " " ++ show rhs'
--- ~ evalExpr n _ = return n
+    applyOp op lhs' rhs'
+
 -- ~ evalExpr st exp = Left $ "unimplemented: " ++ show exp
+
+
 
 evalInput :: ParseTree -> CalcState -> (CalcState, IO())
 evalInput (Expr' expr) st = (st, print $ evalExpr st expr)
 evalInput (Defun (Ident fn,args,body)) st = (CalcState (M.insert fn (args,body) (getFuncs st)) (getVars st)
                                                 , print $ Defun (Ident fn,args,body))
-evalInput (Assignment (Ident idnt,body)) st      = (CalcState (getFuncs st) (M.insert idnt body (getVars st))
-                                                       ,  print $ Assignment (Ident idnt,body))
+evalInput (Assignment (Ident idnt,body)) st = (CalcState (getFuncs st) (M.insert idnt body (getVars st))
+                                              ,  print $ Assignment (Ident idnt,body))  -- NOTE: a=1 b=a a=5 changes b to 5
 evalInput (Error str) st = (st, putStrLn $ "ERROR: " ++ str)
 evalInput expr             st = (st, print expr)
+
+
 
 eval :: [(ParseTree,String)] -> CalcState -> (CalcState, IO ())
 eval []               st = (st, return ())
